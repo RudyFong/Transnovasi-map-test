7cb33728f31165c39ecd8a402cf0551cb4b5a702
RudyFong <rudyfong12@gmail.com>
2025-12-05 13:11:06 +0700

    Update AppDelegate.swift

--- ios/MyNewApps/AppDelegate.swift
+++ ios/MyNewApps/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
-    GMSServices.provideAPIKey("AIzaSyA2DEWsP398ydy32LfiakPB9_q_2gtl8Tk")
+
     reactNativeDelegate = delegate
     reactNativeFactory = factory
 
